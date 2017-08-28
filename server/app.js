a17aed2bd15253dc5e542e46f3832201730f50c8
Erik Niilivirta <erik.niilivirta@siili.com>
2017-08-28 19:01:06 +0300

    removed extra imports

--- server/app.js
+++ server/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./router');
-const db = require('./db/db');
 
 const app = express();
 
-app.use(logger('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 
 router(app);
