fbba7981d74bc4c581a7f8c96bcefc797cd0f414
Erik Niilivirta <erik.niilivirta@siili.com>
2017-08-28 16:55:07 +0300

    passport strategies added

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,11 +1,37 @@
 const passport = require('passport');
-const config = require('../config');
-const db = require('../db/db');
+const bcrypt = require('bcrypt');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+const config = require('../config');
+const db = require('../db/db');
 
-const jwtOptions = {};
 
-const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+const comparePassword = (candidatePassword) => {
+  bcrypt.compare(candidatePassword);
+}
 
+// Local strategy
+const localLogin = new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  const user = db.get('users')
+    .find( email )
+    .value();
 });
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
+
+// JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  const user = db.get('users')
+    .find({ id: payload.sub })
+    .value();
+
+  user !== undefined
+    ? done(null, user)
+    : done(null, false);
+});
+
+passport.use(jwtLogin);
