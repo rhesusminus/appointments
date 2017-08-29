1e4c2d4f0710d64d225c20bd07c0a39ce0976fad
Erik Niilivirta <erik.niilivirta@siili.com>
2017-08-29 20:35:44 +0300

    server: added cors, client: wired redux, actors, etc.

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json({ type: '*/*' }));
+app.use(cors());
 
 router(app);
 
