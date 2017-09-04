a208980659aa5d3aa1956a055f4a3ca0cf0affd9
Erik Niilivirta <erik.niilivirta@siili.com>
2017-09-04 19:13:59 +0300

    add basic user input validation

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -11,32 +11,46 @@ class Signin extends Component {
     this.props.loginUser({ email, password });
   }
 
+  renderField = ({ id, name, input, label, type, meta: { touched, error, warning } }) => {
+    return <div>
+      <div className="mdl-textfield mdl-js-textfield">
+        <input {...input} placeholder={label} type={type} className="mdl-textfield__input" id={id} />
+        <label htmlFor={id} className="mdl-textfield__label">{label}</label>
+      </div>
+      <br />
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  }
+
+  required = (value) => value ? undefined : 'Required';
+  email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Invalid email address' : undefined;
+
   render() {
     const { handleSubmit, errorMessage } = this.props;
 
     return (
       <div className="Signin">
-        {errorMessage && `${errorMessage}`}
+        {errorMessage}
         <form onSubmit={handleSubmit(this.handleSignin)}>
-          <div className="mdl-textfield mdl-js-textfield">
-            <label className="mdl-textfield__label">Email</label>
-            <Field
-              name="email"
-              component="input"
-              type="text"
-              className="mdl-textfield__input"
-            />
-          </div>
+          <Field
+            label="Email"
+            name="email"
+            id="email_input"
+            component={this.renderField}
+            type="text"
+            validate={[ this.required, this.email ]}
+          />
           <br />
-          <div className="mdl-textfield mdl-js-textfield">
-            <label className="mdl-textfield__label">Password</label>
-            <Field
-              name="password"
-              component="input"
-              type="password"
-              className="mdl-textfield__input"
-            />
-          </div>
+          <Field
+            label="Password"
+            name="password"
+            id="password_input"
+            component={this.renderField}
+            type="password"
+            validate={this.required}
+          />
           <br />
           <button
             action="submit"
